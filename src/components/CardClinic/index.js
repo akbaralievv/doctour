cc83cd3bac95388bbbd8ce121d10e14574619bac
User <user@Macbook-Ajdar.local>
2023-05-29 01:16:56 +0600

    fixed the bug with the icon also put everything in the container

--- src/components/CardClinic/index.js
+++ src/components/CardClinic/index.js
@@ -22,7 +22,7 @@ function CardClinic() {
             return (
                 <>
                     <span onClick={handleClick}>
-                        Подробнее <img src="src/assets/icons/CardClinic/arrow-down.svg" alt="arrow-down icon" />
+                        Подробнее <img src="/src/assets/icons/CardClinic/arrow-down.svg" alt="arrow-down icon" />
                     </span>
                 </>
             );
@@ -31,7 +31,7 @@ function CardClinic() {
 
     return (
         <div className="CardClinic">
-            <div className="container">
+            <div className={styles.container}>
                 <div className={styles.CardClinic_inner}>
                     <div className={styles.CardClinic_logo}>
                         <img src="src/assets/Images/CardClinic/Unimed-Clinic.png" alt="Unimed-Clinic" />
@@ -51,19 +51,19 @@ function CardClinic() {
 
                     <div className={styles.CardClinic_information}>
                         <div className={styles.CardClinic_information_cards}>
-                            <img src="src/assets/icons/CardClinic/Location.svg" alt="Location icon" />
+                            <img src="/src/assets/icons/CardClinic/Location.svg" alt="Location icon" />
                             <span>г.Бишкек, ул. Байтик Баатыра 3в</span>
                         </div>
 
                         <div className={styles.CardClinic_information_cards}>
-                            <img src="src/assets/icons/CardClinic/Discovery.svg" alt="Discovery icon" />
-                            <a href="https://2gis.kg/bishkek/inside/15763234351045008/firm/70000001029245818?m=74.608751%2C42.835966%2F19.15%2Fr%2F-3.33">
+                            <img src="/src/assets/icons/CardClinic/Discovery.svg" alt="Discovery icon" />
+                            <a href="https://2gis.kg/bishkek/inside/15763234351045008/firm/70000001029245818?m=74.608751%2C42.835966%2F19.15%2Fr%2F-3.33" target="blank">
                                 мы в 2ГИС
                             </a>
                         </div>
 
                         <div className={styles.CardClinic_information_cards}>
-                            <img src="src/assets/icons/CardClinic/Time-Circle.svg" alt="" />
+                            <img src="/src/assets/icons/CardClinic/Time-Circle.svg" alt="Time-Circle icon" />
                             <ul className={styles.information_cards_ul}>
                                 <li>ПН - ПТ  8.00 - 18.00</li>
                                 <li>СБ - ВС   9.00 - 16.00</li>
@@ -71,7 +71,7 @@ function CardClinic() {
                         </div>
 
                         <div className={`${styles.CardClinic_information_cards} ${styles.CardClinic_information_cardsTwo}`}>
-                            <img src="src/assets/icons/CardClinic/Calling.svg" alt="" />
+                            <img src="/src/assets/icons/CardClinic/Calling.svg" alt="Calling icon" />
                             <ul className={styles.information_cards_ul}>
                                 <li className={styles.information_cards_li_strong}>+996 312 000 111</li>
                                 <li className={styles.information_cards_li_strong}>+996 555 000 111</li>
@@ -85,4 +85,3 @@ function CardClinic() {
 }
 
 export default CardClinic;
-    
