578f3820716a13dbe13826bb6ea9ab17facf0ffd
AkbaralievAlisher <kgkgg28@gmail.com>
2023-05-31 13:40:24 +0600

    m

--- src/pages/DoctorsPage/index.js
+++ src/pages/DoctorsPage/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import AboutDoctor from "../../components/AboutDoctor";
-// import BtnForModal from "../../components/BtnForModal";
+import AboutDoctor from '../../components/AboutDoctor';
+import BtnForModal from '../../components/BtnForModal';
 
 function DoctorsPage() {
   return (
-      <div>
-            {/*<AboutDoctor />*/}
-            {/*<BtnForModal/>*/}
-      </div>
+    <div>
+      <AboutDoctor />
+      <BtnForModal />
+    </div>
   );
 }
 
