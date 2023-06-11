93d4a866316bc357bc87fe276ae99fcb6a53a222
User <user@Macbook-Ajdar.local>
2023-06-11 14:11:29 +0600

    Updated some styles in CardClinic

--- src/components/CardClinic/index.js
+++ src/components/CardClinic/index.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 import styles from './CardClinic.module.css';
+import CardClinicImage from '/src/assets/images/CardClinic/Unimed-Clinic.png';
+import arrowUpIcon from '/src/assets/icons/CardClinic/arrow-up.svg';
+import arrowDownIcon from '/src/assets/icons/CardClinic/arrow-down.svg';
+import discoveryIcon from '/src/assets/icons/CardClinic/Discovery.svg';
+import locationIcon from '/src/assets/icons/CardClinic/Location.svg';
+import timeCircleIcon from '/src/assets/icons/CardClinic/Time-Circle.svg';
+import callingIcon from '/src/assets/icons/CardClinic/Calling.svg';
+
 
 function CardClinic() {
     const [expanded, setExpanded] = useState(false);
@@ -14,7 +22,7 @@ function CardClinic() {
                 <>
                     Врачи высшей категории окажут Вам высококвалифицированную помощь по всем основным направлениям{' '}
                     <span onClick={handleClick}>
-                        Поменьше <img src="/src/assets/icons/CardClinic/arrow-up.svg" alt="arrow-up icon" />
+                        Поменьше <img src={arrowUpIcon} alt="arrow-up icon" />
                     </span>
                 </>
             );
@@ -22,7 +30,7 @@ function CardClinic() {
             return (
                 <>
                     <span onClick={handleClick}>
-                        Подробнее <img src="/src/assets/icons/CardClinic/arrow-down.svg" alt="arrow-down icon" />
+                        Подробнее <img src={arrowDownIcon} alt="arrow-down icon" />
                     </span>
                 </>
             );
@@ -34,7 +42,7 @@ function CardClinic() {
             <div className={styles.container}>
                 <div className={styles.CardClinic_inner}>
                     <div className={styles.CardClinic_logo}>
-                        <img src="src/assets/Images/CardClinic/Unimed-Clinic.png" alt="Unimed-Clinic" />
+                        <img src={CardClinicImage} alt="Unimed-Clinic" />
                     </div>
 
                     <div className={styles.CardClinic_title}>
@@ -51,19 +59,17 @@ function CardClinic() {
 
                     <div className={styles.CardClinic_information}>
                         <div className={styles.CardClinic_information_cards}>
-                            <img src="/src/assets/icons/CardClinic/Location.svg" alt="Location icon" />
+                            <img src={locationIcon} alt="Location icon" />
                             <span>г.Бишкек, ул. Байтик Баатыра 3в</span>
                         </div>
 
                         <div className={styles.CardClinic_information_cards}>
-                            <img src="/src/assets/icons/CardClinic/Discovery.svg" alt="Discovery icon" />
-                            <a href="https://2gis.kg/bishkek/inside/15763234351045008/firm/70000001029245818?m=74.608751%2C42.835966%2F19.15%2Fr%2F-3.33" target="blank">
-                                мы в 2ГИС
-                            </a>
+                            <img src={discoveryIcon} alt="Discovery icon" />
+                            <a href="https://2gis.kg/bishkek/inside/15763234351045008/firm/70000001029245818?m=74.608751%2C42.835966%2F19.15%2Fr%2F-3.33" target="blank">мы в 2ГИС</a>
                         </div>
 
                         <div className={styles.CardClinic_information_cards}>
-                            <img src="/src/assets/icons/CardClinic/Time-Circle.svg" alt="Time-Circle icon" />
+                            <img src={timeCircleIcon} alt="Time-Circle icon" />
                             <ul className={styles.information_cards_ul}>
                                 <li>ПН - ПТ  8.00 - 18.00</li>
                                 <li>СБ - ВС   9.00 - 16.00</li>
@@ -71,7 +77,7 @@ function CardClinic() {
                         </div>
 
                         <div className={`${styles.CardClinic_information_cards} ${styles.CardClinic_information_cardsTwo}`}>
-                            <img src="/src/assets/icons/CardClinic/Calling.svg" alt="Calling icon" />
+                            <img src={callingIcon} alt="Calling icon" />
                             <ul className={styles.information_cards_ul}>
                                 <li className={styles.information_cards_li_strong}>+996 312 000 111</li>
                                 <li className={styles.information_cards_li_strong}>+996 555 000 111</li>
@@ -85,3 +91,4 @@ function CardClinic() {
 }
 
 export default CardClinic;
+    
