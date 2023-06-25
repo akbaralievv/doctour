7793e1e704a288f722f9ea85557464311d7baaad
User <user@Macbook-Ajdar.local>
2023-06-25 14:24:33 +0600

    Made the ErrorPage page

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './ErrorPage.module.css';
+
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+    return ( 
+        <div className={styles.mainBlock}>
+            <div className={styles.blockError}>
+                <h1>404</h1>
+                <p>Ой, страница не найдена</p>
+            </div>
+        </div>
+     );
 }
 
 export default ErrorPage;
